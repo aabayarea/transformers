4547b6876a3fa2c200025de583b0d0975a647339
AA <37912140+aabayarea@users.noreply.github.com>
2021-08-25 21:54:28 -0700

    Update run_translation.py

--- examples/pytorch/translation/run_translation.py
+++ examples/pytorch/translation/run_translation.py
@@ -305,7 +305,7 @@ def main():
     if data_args.dataset_name is not None:
         # Downloading and loading a dataset from the hub.
         raw_datasets = load_dataset(
-            data_args.dataset_name, data_args.dataset_config_name, data_dir="wmt_en_ro", cache_dir=model_args.cache_dir
+            data_args.dataset_name, data_args.dataset_config_name, cache_dir=model_args.cache_dir
         )
     else:
         data_files = {}
@@ -318,7 +318,7 @@ def main():
         if data_args.test_file is not None:
             data_files["test"] = data_args.test_file
             extension = data_args.test_file.split(".")[-1]
-        raw_datasets = load_dataset(extension, data_files=data_files, data_dir="wmt_en_ro", cache_dir=model_args.cache_dir)
+        raw_datasets = load_dataset(extension, data_files=data_files, cache_dir=model_args.cache_dir)
     # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
 
